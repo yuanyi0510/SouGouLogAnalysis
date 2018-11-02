e81dc2f69a484407813cd1aa437dee329aafb808
yuanyi0510 <1096751192@qq.com>
2018-11-02 14:04:48 +0800

    修改错误

--- src/main/java/com/hadoop/mapreduce/counturl/SortUrlAnalysis.java
+++ src/main/java/com/hadoop/mapreduce/counturl/SortUrlAnalysis.java
@@ -4,9 +4,10 @@ import org.apache.hadoop.util.ToolRunner;
 
 public class SortUrlAnalysis {
     public static void main(String[] args) throws Exception {
-        int statuscount = ToolRunner.run(new UrlCountMapReduce(), new String[]{args[0], args[1]});
+        UrlCountMapReduce urlCountMapReduce = new UrlCountMapReduce();
+        int statuscount = ToolRunner.run(urlCountMapReduce, new String[]{args[0], args[1]});
         if (statuscount != -1) {
-            int statussort = ToolRunner.run(new SortUrlMapReduce(SortUrlMapReduce.SortUrlReducer.getTOTAL()),
+            int statussort = ToolRunner.run(new SortUrlMapReduce(urlCountMapReduce.getTotal()),
                     new String[]{args[1], args[2]});
             System.exit(statussort);
         } else {
